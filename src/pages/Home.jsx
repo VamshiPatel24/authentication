eb48982edc6adab9fe41d0c52e0411ddbcffd675
VamshiPatel24 <vamshicheepirishetti@gmail.com>
2025-05-07 11:08:26 +0530

    Sign out added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import {signOut } from "firebase/auth"
+import {auth} from '../Firebase'
+import {useNavigate} from 'react-router-dom'
 function home(){
+  const navigate=useNavigate()
+  const logOutUser=()=>{
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      alert('Logged out successful')
+      navigate('/')
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
     return(
       <>
         <h1>home</h1>
+        <button onClick={logOutUser}>Log out</button>
       </>
     )
   }
